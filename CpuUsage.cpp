207c58a57b77db6c4e1ea5d24288cc63a11f62a1
francium503 <37111673+francium503@users.noreply.github.com>
2019-04-22 20:26:24 +0900

    cpu 사용률 클래스 추가

---
+++ CpuUsage.cpp
@@ -0,0 +1,121 @@
+#include "pch.h"
+#include "CpuUsage.h"
+
+
+CpuUsage::CpuUsage()
+{
+	hProcess = GetCurrentProcess();
+
+	SYSTEM_INFO si;
+
+	GetSystemInfo(&si);
+
+	iNumberofProcessors = si.dwNumberOfProcessors;
+
+	fProcessorTotal = 0;
+	fProcessorUser = 0;
+	fProcessorKernel = 0;
+
+	fProcessTotal = 0;
+	fProcessUser = 0;
+	fProcessKernel = 0;
+
+	ftProcessor_LastKernel.QuadPart = 0;
+	ftProcessor_LastUser.QuadPart = 0;
+	ftProcessor_LastIdle.QuadPart = 0;
+
+	ftProcess_LastUser.QuadPart = 0;
+	ftProcess_LastKernel.QuadPart = 0;
+	ftProcess_LastTime.QuadPart = 0;
+
+	UpdateCpuTime();
+}
+
+CpuUsage::CpuUsage(HANDLE hProcess)
+{
+	hProcess = hProcess;
+
+	SYSTEM_INFO si;
+
+	GetSystemInfo(&si);
+
+	iNumberofProcessors = si.dwNumberOfProcessors;
+
+	fProcessorTotal = 0;
+	fProcessorUser = 0;
+	fProcessorKernel = 0;
+
+	fProcessTotal = 0;
+	fProcessUser = 0;
+	fProcessKernel = 0;
+
+	ftProcessor_LastKernel.QuadPart = 0;
+	ftProcessor_LastUser.QuadPart = 0;
+	ftProcessor_LastIdle.QuadPart = 0;
+
+	ftProcess_LastUser.QuadPart = 0;
+	ftProcess_LastKernel.QuadPart = 0;
+	ftProcess_LastTime.QuadPart = 0;
+
+	UpdateCpuTime();
+}
+
+void CpuUsage::UpdateCpuTime(void)
+{
+	ULARGE_INTEGER idle;
+	ULARGE_INTEGER kernel;
+	ULARGE_INTEGER user;
+
+	if (GetSystemTimes((PFILETIME)&idle, (PFILETIME)&kernel, (PFILETIME)&user) == false) {
+		return;
+	}
+
+	ULONGLONG kernelDiff = kernel.QuadPart - ftProcessor_LastKernel.QuadPart;
+	ULONGLONG userDiff = user.QuadPart - ftProcessor_LastUser.QuadPart;
+	ULONGLONG idleDiff = idle.QuadPart - ftProcessor_LastIdle.QuadPart;
+
+	ULONGLONG total = kernelDiff + userDiff;
+	ULONGLONG timeDiff;
+
+	if (total == 0) {
+		fProcessorUser = 0.0f;
+		fProcessorKernel = 0.0f;
+		fProcessorTotal = 0.0f;
+	}
+	else {
+		fProcessorTotal = (float)((double)(total - idleDiff) / total * 100.0f);
+		fProcessorUser = (float)((double)userDiff / total * 100.0f);
+		fProcessorKernel = (float)((double)(kernelDiff - idleDiff) / total * 100.0f);
+	}
+
+	ftProcessor_LastKernel = kernel;
+	ftProcessor_LastUser = user;
+	ftProcessor_LastIdle = idle;
+
+	ULARGE_INTEGER none;
+	ULARGE_INTEGER nowTime;
+
+	GetSystemTimeAsFileTime((LPFILETIME)&nowTime);
+
+	GetProcessTimes(hProcess, (LPFILETIME)&none, (LPFILETIME)&none, (LPFILETIME)&kernel, (LPFILETIME)&user);
+
+	timeDiff = nowTime.QuadPart - ftProcess_LastTime.QuadPart;
+	userDiff = user.QuadPart - ftProcess_LastUser.QuadPart;
+	kernelDiff = kernel.QuadPart - ftProcess_LastKernel.QuadPart;
+
+	total = kernelDiff + userDiff;
+
+	fProcessTotal = (float)(total / (double)iNumberofProcessors / (double)timeDiff * 100.0f);
+	fProcessKernel = (float)(kernelDiff / (double)iNumberofProcessors / (double)timeDiff * 100.0f);
+	fProcessUser = (float)(userDiff / (double)iNumberofProcessors / (double)timeDiff * 100.0f);
+
+	ftProcess_LastTime = nowTime;
+	ftProcess_LastKernel = kernel;
+	ftProcess_LastUser = user;
+
+}
+
+
+CpuUsage::~CpuUsage()
+{
+}
---
+++ CpuUsage.h
@@ -0,0 +1,41 @@
+#pragma once
+
+class CpuUsage
+{
+public:
+	CpuUsage();
+	CpuUsage(HANDLE hProcess);
+
+	void UpdateCpuTime(void);
+
+	float ProcessorTotal(void) { return fProcessorTotal; }
+	float ProcessorUser(void) { return fProcessorUser; }
+	float ProcessorKernel(void) { return fProcessorKernel; }
+
+	float ProcessTotal(void) { return fProcessTotal; }
+	float ProcessUser(void) { return fProcessUser; }
+	float ProcessKernel(void) { return fProcessKernel; }
+
+	~CpuUsage();
+
+private:
+	HANDLE hProcess;
+	int iNumberofProcessors;
+
+	float fProcessorTotal;
+	float fProcessorUser;
+	float fProcessorKernel;
+
+	float fProcessTotal;
+	float fProcessUser;
+	float fProcessKernel;
+
+	ULARGE_INTEGER ftProcessor_LastKernel;
+	ULARGE_INTEGER ftProcessor_LastUser;
+	ULARGE_INTEGER ftProcessor_LastIdle;
+
+	ULARGE_INTEGER ftProcess_LastKernel;
+	ULARGE_INTEGER ftProcess_LastUser;
+	ULARGE_INTEGER ftProcess_LastTime;
+};
+
