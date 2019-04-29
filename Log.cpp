8e31578b12253a7983544365fc54b9cddcc76c84
francium503 <37111673+francium503@users.noreply.github.com>
2019-04-29 12:47:40 +0900

    Log 작성중

--- Log.cpp
+++ Log.cpp
@@ -1,2 +1,18 @@
+#include "pch.h"
 #include "Log.h"
 
+void NetLib::Log::SysLog(WCHAR * szType, eLogLevel logLevel, WCHAR * szStringFormat, ...)
+{
+	if (logLevel < _logLevel) {
+		return;
+	}
+
+	WCHAR LogString[1024];
+	va_list vaList;
+	va_list value;
+
+	va_start(vaList, szStringFormat);
+	va_copy(value, vaList);
+
+	HRESULT result = StringCbPrintf(LogString, 1024 * sizeof(WCHAR), szStringFormat, );
+}
--- Log.h
+++ Log.h
@@ -4,5 +4,29 @@ namespace NetLib {
 
 	class Log {
 
+		enum class eLogLevel
+		{
+			eLogDebug,
+			eLogWarning,
+			eLogError,
+			eLogSystem,
+		};
+
+	public:
+		Log();
+		Log(WCHAR *szDir);
+		~Log();
+
+		void LogDir(WCHAR *szDir);
+		void SysLog(WCHAR *szType, eLogLevel logLevel, WCHAR* szStringFormat, ...);
+		void SysLogHex(WCHAR *szType, eLogLevel logLevel, WCHAR *szLog, BYTE *pByte, int iByteLen);
+
+	private:
+		WCHAR *_szDir;
+		BOOL _consoleLog;
+		eLogLevel _logLevel;
+		SRWLOCK _srwLock;
+
+
 	};
 }
